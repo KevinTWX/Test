31a3a7f67a23d93170d26725591d3f490065c85d
KevinTWX <44801036+KevinTWX@users.noreply.github.com>
2018-11-07 15:28:40 +0800

    Update Tt.java

--- Testc/src/main/java/www/yi/Tt.java
+++ Testc/src/main/java/www/yi/Tt.java
@@ -3,8 +3,8 @@ package www.yi;
 public class Tt {
 public static void main(String[] args) {
 	System.out.println("怡亚通");
-	System.out.println("怡亚通111111111");
-	System.out.println("怡亚通222222222");
+	System.out.println("222222");
+	System.out.println("0000000000000");
 	System.out.println("怡亚通33333333");
 }
 }
