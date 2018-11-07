1307209de09ea4c445af40e2d3e295277db86670
gitKevinTWX <15676220920@163.com>
2018-11-07 15:15:14 +0800

    Git01

---
+++ GitAsia/src/main/java/com/asia/GitTest01.java
@@ -0,0 +1,13 @@
+package com.asia;
+
+public class GitTest01 {
+
+	public static void main(String[] args) {
+		System.out.println("怡亚通");
+		System.out.println("怡亚通");
+		System.out.println("怡亚通");
+		System.out.println("怡亚通");
+		System.out.println("怡亚通");
+		System.out.println("怡亚通");
+	}
+}
